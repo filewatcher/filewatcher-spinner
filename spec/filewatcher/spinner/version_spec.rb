3099393c36040179cdb73e9f15825e7c583fa0e3
Alexander Popov <alex.wayfer@gmail.com>
2022-06-25 20:02:25 +0300

    Allow `beta` in version spec

--- spec/filewatcher/spinner/version_spec.rb
+++ spec/filewatcher/spinner/version_spec.rb
@@ -3,5 +3,5 @@
 describe 'Filewatcher::Spinner::VERSION' do
   subject { Object.const_get(self.class.description) }
 
-  it { is_expected.to match(/^\d+\.\d+\.\d+$/) }
+  it { is_expected.to match(/^\d+\.\d+\.\d+(\.beta\d+)?$/) }
 end
